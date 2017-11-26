9c5e8dd3e599f23e45bedc7c1fd91c9c6635d61c
Flex <lumodder@gmail.com>
2017-11-26 09:15:51 -0700

    luna cant js

--- utils.js
+++ utils.js
@@ -106,7 +106,9 @@ utils.lookupUser = function(ctx,msg,str){
 
 function timeString(){
 	let d = new Date();
-	let h, m, s = d.getHours(), d.getMinutes(), d.getSeconds();
+	let h = d.getHours();
+	let m = d.getMinutes();
+	let s = d.getSeconds();
 	return (h < 10 ? "0"+h : h)+":"+(m < 10 ? "0"+m : m)+":"+(s < 10 ? "0"+s : s);
 }
 
